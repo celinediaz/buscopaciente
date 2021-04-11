f82cb984bd6e99eddd81e6a31bd151cf2e09fca3
celinediaz <38956985+celinediaz@users.noreply.github.com>
2021-04-11 11:56:44 -0500

    conflicts
    Resolved conflicts for App.js ?

--- src/App.js
+++ src/App.js
@@ -6,12 +6,9 @@ import Signup from './components/Signup';
 import VerCitas from './components/VerCitas';
 import AgendarCita from './components/AgendarCita';
 import LandingPage from './components/LandingPage';
-<<<<<<< Updated upstream
-=======
 import MenuUsuarios from './components/MenuUsuarios';
 import MenuExperto from './components/MenuExperto';
 import ExpRoute from './components/ExpRoute';
->>>>>>> Stashed changes
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { AuthProvider } from "./contexts/AuthContext";
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
@@ -24,23 +21,6 @@ function App() {
     <AuthProvider>
       <Navb />
         <Switch>
-<<<<<<< Updated upstream
-          <Route exact path="/">
-            < LandingPage />
-          </Route>
-          <Route path="/signup">
-            < Signup />
-          </Route>
-          <Route path="/login">
-            < Login />
-          </Route>
-          <Route path="/vercitas">
-            < VerCitas />
-          </Route>
-          <Route path="/agendarcitas">
-            < AgendarCita />
-          </Route>
-=======
           <Route exact path="/" component = {LandingPage}/>
           <Route path="/signup" component ={Signup}/>
           <Route path="/signuppract" component ={SignupPract}/>
@@ -51,7 +31,6 @@ function App() {
           <Route path="/expediente" component = {ExpedienteIndividual} />
           <Route path="/usuario" component = {MenuUsuarios}/>
           <ExpRoute path="/experto" component={MenuExperto}/>
->>>>>>> Stashed changes
         </Switch>
         </AuthProvider>
       <Footer />
